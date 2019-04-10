6e5f999e2a433f7a65377b99a6aa0cf91e443133
Razvan <razvanrus289@yahoo.com>
2019-04-10 12:57:33 -0500

    added javascript for the button and h2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function init(){
-//add your javascrip between these two lines of code
+function init() {
+  alert("Razvan Rus: " + message);
+}
+var button = document.getElementById('entrybutton');
+button.addEventListener('click', init);
 
+var message = document.getElementById("entryinput").value;
+document.getElementById("textoutput").value = message;
 
+window.addEventListener('load', init);
 
-
-
-
-
-
-    window.addEventListener('load', init);
