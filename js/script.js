2fc4bb1d71806f5e9e62185e1e03c9b2d5be3b67
Razvan <razvanrus289@yahoo.com>
2019-04-10 17:41:47 -0500

    fixed the javascript and got it to work

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 function init() {
-  alert("Razvan Rus: " + message);
+
+var button = document.getElementById("entrybutton");
+
+var message = document.getElementById("entryinput");
+
+button.addEventListener("click", function() {
+  alert ("Razvan Rus: " + message.value);
+  document.getElementById("textoutput").innerHTML = message.value;
+
+});
+
+
+
 }
-var button = document.getElementById('entrybutton');
-button.addEventListener('click', init);
-
-var message = document.getElementById("entryinput").value;
-document.getElementById("textoutput").value = message;
-
 window.addEventListener('load', init);
+
 
